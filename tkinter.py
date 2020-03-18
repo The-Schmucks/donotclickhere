5bb33be4eaad761d40ab4b1913e0ec7aead083ad
munezaclovis <mugaro234@gmail.com>
2020-03-18 04:23:56 -0400

    just fucked around

--- tkinter.py
+++ tkinter.py
@@ -12,27 +12,25 @@ def TextInput(prompt):
 
 def answering(dictionary, data):
     conv = data
-    for key, value in dictionary.items():
-        if conv in dictionary:
-            if str(conv) == str(key):
-                if conv == 'kina':
-                    search_term = input("Kadogo: Mbwira izina ry'indirimbo: ")
-                    results = YoutubeSearch(search_term, max_results=10).to_json()
+    if conv in dictionary:
+        if str(conv) in dictionary:
+            value = dictionary.get(conv)
+            if conv == 'kina':
+                search_term = input("Kadogo: Mbwira izina ry'indirimbo: ")
+                results = YoutubeSearch(search_term, max_results=10).to_json()
+                i = results.find('link')
+                j = results.find(',', i, -1)
 
-                    i = results.find('link')
-                    j = results.find(',', i, -1)
-    
-                    needed = results[i: j]
-                    i1 = needed.find('/')
-                    needed2 = needed[i1: -1]
-
-                    a_website = "https://www.youtube.com" + needed2
-                    answer = webbrowser.open_new(a_website)
-                answer = str(value)
-            
-        else:
-            answer = 'Ntago mbyumvise !'
-            
+                needed = results[i: j]
+                i1 = needed.find('/')
+                needed2 = needed[i1: -1]
+                a_website = "https://www.youtube.com" + needed2
+                answer = webbrowser.open_new(a_website)
+            answer = str(value)
+        
+    else:
+        answer = 'Ntago mbyumvise !'
+        
     return answer 
 
 def read(filename):
